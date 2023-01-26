e372a115b5a0696f0d09ee5c854a803da39779fc
AlgoWala1 <120630438+AlgoWala1@users.noreply.github.com>
2023-01-26 17:18:02 +0530

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -1,4 +1,6 @@
 def isPrime(i):
+    if i == 1:
+        return False
     for j in range(2,i):
         if i%j == 0:
             return False
@@ -7,7 +9,5 @@ def isPrime(i):
 n1= int(input("Enter lower range:"))
 n2 = int(input("Enter higher range:"))
 for i in range(n1,n2+1):
-    if i == 2:
-        print("2")
     if isPrime(i):
         print(i)
